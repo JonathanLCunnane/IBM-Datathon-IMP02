1be4a22abacf3fde9331a186b8e3a7400c046f4b
Jonathan Cunnane <jc4423@ic.ac.uk>
2024-10-19 12:48:58 +0100

    Started work on web scraping. Buttons disabling and re-enabling.

--- extension/index.js
+++ extension/index.js
@@ -1,20 +1,69 @@
-const scanButton = document.getElementById('scanButton');
+const scanAllButton = document.getElementById("scanAllButton");
+const scanTextButton = document.getElementById("scanTextButton");
+const scanImagesButton = document.getElementById("scanImagesButton");
+const scanVideosButton = document.getElementById("scanVideosButton");
+const scanButtons = [scanAllButton, scanTextButton, scanImagesButton, scanVideosButton];
 
-scanButton.addEventListener('click', scanPage);
+scanAllButton.addEventListener("click", scanAll);
+scanTextButton.addEventListener("click", scanText);
+scanImagesButton.addEventListener("click", scanImages);
+scanVideosButton.addEventListener("click", scanVideos);
 
-const resultsDiv = document.getElementById('result');
+function scanAll() {
+    disableButtons();
+    scanText();
+}
 
-function scanPage() {
-    scanButton.disabled = true;
-    scanButton.innerHTML = 'Loading...';
+function scanText() {
+    disableButtons()
     chrome.tabs.query({active: true, currentWindow: true})
-    .then(tabs => {
-        chrome.tabs.sendMessage(tabs[0].id, {action: 'scanPage'})
-    })
-    .then(result => {
-        console.log(result);
-        resultsDiv.classList.toggle('hidden');
-        scanButton.innerHTML = 'Scan Complete';
+        .then(tabs => {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "scanText"})
+        })
+        .then(result => {
+            enableButtons();
+        });
+}
+
+function scanImages() {
+    disableButtons();
+    // TODO
+    enableButtons();
+}
+
+function scanVideos() {
+    disableButtons();
+    // TODO
+    enableButtons();
+}
+
+function disableButtons() {
+    scanButtons.forEach(button => {
+        button.disabled = true;
     });
+}
+
+function enableButtons() {
+    scanButtons.forEach(button => {
+        button.disabled = false;
+    });
+}
+
+function scanPage(scanAction) {
+    scanButtons.forEach(button => {
+        button.disabled = true;
+    });
+
+    // scanButton.disabled = true;
+    // scanButton.innerHTML = 'Loading...';
+    // chrome.tabs.query({active: true, currentWindow: true})
+    // .then(tabs => {
+    //     chrome.tabs.sendMessage(tabs[0].id, {action: 'scanPage'})
+    // })
+    // .then(result => {
+    //     console.log(result);
+    //     resultsDiv.classList.toggle('hidden');
+    //     scanButton.innerHTML = 'Scan Complete';
+    // });
 }
 
