d438c918d5d87d6940060ad8ea7a9c95438454f6
Jonathan Cunnane <jc4423@ic.ac.uk>
2024-10-19 22:28:39 +0100

    Started work on fetching

--- extension/content.js
+++ extension/content.js
@@ -2,10 +2,14 @@ chrome.runtime.onMessage.addListener(async function(request, sender, sendRespons
     switch (request.action) {
         case "scanText":
             const textData = await scanText();
-            sendResponse({data: textData});
+            sendResponse(textData);
+            break;
+        case "scrapeImages":
+            sendResponse({images: scrapeImages()});
             break;
         case "scanImages":
-            sendResponse({images: scanImages()});
+            const imagesData = await scanImages();
+            sendResponse(imagesData);
             break;
         case "scanImage":
             const scanData = await scanImage(request.imgUrl);
@@ -17,6 +21,7 @@ chrome.runtime.onMessage.addListener(async function(request, sender, sendRespons
 async function scanText() {
     let textList = [];
     let elements = [];
+    let scores = [];
 
     $("p, h1, h2, h3, h4, h5, h6, blockquote").each(function() {
         if ($(this).text().length < 80) {
@@ -37,6 +42,7 @@ async function scanText() {
         });
 
         const data = await response.json();
+        scores = data.scores;
 
         // Assume `data` contains an array of scores corresponding to the text blocks
         data.scores.forEach((fakeScore, index) => {
@@ -51,20 +57,33 @@ async function scanText() {
             }
         });
 
-        return data;  // Return the response from the server
+        return { status: "OK", text: textList, scores: data.scores }; // Return the response from the server
 
     } catch (error) {
         console.error("Error sending text to the server:", error);
-        return { error: "Failed to send text to the server" };
+        return { status: "Failed to send text to the server" };
     }
 }
 
-function scanImages() {
+function scrapeImages() {
     let images = [];
     $("img").each(function() {
         images.push($(this).attr("src"));
     });
     return images;
+}
+
+async function scanImages() {
+    const images = scrapeImages();
+    const scores = [];
+    for (let img in images) {
+        const imgFakeScore = await scanImage(img);
+        if (imgFakeScore.status !== "OK") {
+            return { status: "Failed to scan an image" };
+        }
+        scores.push(imgFakeScore);
+    }
+    return { status: "OK", scores: scores, images: images };
 }
 
 // Modified scanImage to send image URL to the server and get a fake score
@@ -81,12 +100,13 @@ async function scanImage(targetUrl) {
         const data = await response.json();
 
         // TODO: Change this to the actual key used in the response
-        const fakeScore = data.fakeScore;
+        const fakeScore = Math.random();
         console.log("Image scan result:", fakeScore);
-        return fakeScore;
+        return { status: "OK", score: fakeScore };
 
     } catch (error) {
         console.error("Error sending image to the server:", error);
+        return { status: "Failed to send image to the server" };
     }
 }
 
